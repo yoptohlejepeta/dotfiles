996216d609c8467f46fc863da6a99068dc50de7c
yoptohlejepeta <peta.kotlan@seznam.cz>
2025-11-19 20:10:29 +0100

    fix template path

--- nix-templates/flake.nix
+++ nix-templates/flake.nix
@@ -1,14 +1,14 @@
 {
   description = "My Nix templates";
-  
+
   outputs = { self }: {
     templates = {
       simple-packages = {
-        path = ./simple-packages;
+        path = ./basic;
         description = "Simple universal package template";
       };
     };
-    
+
     defaultTemplate = self.templates.basic;
   };
 }
