273bc8738966991a2660aaa37c35eb8e826bdd5b
yoptohlejepeta <peta.kotlan@seznam.cz>
2025-11-19 20:15:37 +0100

    right

--- nix-templates/basic/flake.nix
+++ nix-templates/basic/flake.nix
@@ -1,20 +1,19 @@
 {
-  description = "My packages";
-
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
-
+  description = "My project";
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
   outputs = { nixpkgs, ... }:
     let
       eachSystem = f:
         nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed
         (system: f nixpkgs.legacyPackages.${system});
     in {
-      packages = eachSystem (pkgs:
-        {
-          # Add your packages here
-        });
-
-      devShells =
-        eachSystem (pkgs: { default = pkgs.mkShell { buildInputs = [ ]; }; });
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [
+              # packages
+            ];
+        };
+      });
     };
 }
